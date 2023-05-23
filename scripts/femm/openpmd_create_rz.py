2c97fd458d813d8a39131c27783ee51181fefc9a
Axel Huebl <axel.huebl@plasma.ninja>
2023-05-23 15:48:37 -0700

    CI: Validate HDF5 Files (#17)
    * CI: Validate HDF5 Files
    
    Run the validator on HDF5 files.
    
    * Fix: FEMM

--- scripts/femm/openpmd_create_rz.py
+++ scripts/femm/openpmd_create_rz.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python3
 import openpmd_api as io
 import numpy as np
 
 # number of grid points
 # nr*nz = the number of lines in B*_rz.txt
+num_nodes = 1
 nr = 47
 nz = 47
 # read data
-Br_data = np.loadtxt('Br_rz.txt').reshape(1, nr, nz)
-Bz_data = np.loadtxt('Bz_rz.txt').reshape(1, nr, nz)
+Br_data = np.loadtxt('Br_rz.txt').reshape(num_nodes, nr, nz)
+Bz_data = np.loadtxt('Bz_rz.txt').reshape(num_nodes, nr, nz)
 
 # create openpmd file
 series = io.Series("example-femm-thetaMode.h5",io.Access.create)
@@ -16,10 +18,12 @@ it = series.iterations[1]
 
 # set meta information
 B = it.meshes["B"]
-B.grid_spacing = [0.025, 0.125]
+B.grid_spacing = np.array([0.025, 0.125])
 B.grid_global_offset = [0.0, -0.375]
 B.axis_labels = ['r', 'z']
 B.geometry = io.Geometry.thetaMode
+B.geometry_parameters = "m=" + str(num_nodes) + ";imag=+"
+
 B.unit_dimension = {
     io.Unit_Dimension.M:  1,
     io.Unit_Dimension.I: -1,
@@ -45,10 +49,11 @@ B_z.store_chunk(Bz_data)
 
 # set meta information
 E = it.meshes["E"]
-E.grid_spacing = [0.025, 0.125]
+E.grid_spacing = np.array([0.025, 0.125])
 E.grid_global_offset = [0.0, -0.375]
 E.axis_labels = ['r', 'z']
 E.geometry = io.Geometry.thetaMode
+E.geometry_parameters = "m=" + str(num_nodes) + ";imag=+"
 E.unit_dimension = {
     io.Unit_Dimension.M:  1,
     io.Unit_Dimension.L:  1,
